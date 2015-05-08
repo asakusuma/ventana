03b0522775eb7484ffa816eda35176daac90a0de
asakusuma <asa.kusuma@gmail.com>
2015-05-07 17:34:56 -0700

    Update dependencies and bump

--- exports/ventana.js
+++ exports/ventana.js
@@ -10,11 +10,11 @@
     // like Node.
     module.exports = factory();
   } else {
-    root['@linkedin/ventana'] = factory();
+    root['ventana'] = factory();
   }
 }(this, function () {
 
-_(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"@linkedin/ventana":[function(_dereq_,module,exports){
+_(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"ventana":[function(_dereq_,module,exports){
 'use strict';
 
 var listenerMap = {
@@ -47,7 +47,7 @@ document.addEventListener('visibilitychange', function() {
   }
 });
 
-exports['default'] = {
+module.exports = {
   on: function(e, callback) {
     var listeners = listenerMap[e];
     if (listeners) {
@@ -77,8 +77,8 @@ exports['default'] = {
     };
   }
 };
-},{}]},{},["@linkedin/ventana"]);
+},{}]},{},["ventana"]);
 
-return _require("@linkedin/ventana");
+return _require("ventana");
 
 }));
