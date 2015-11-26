3e4adb1e0ff188b0f02d7c52425fa63906ceb3cc
asakusuma <asa.kusuma@gmail.com>
2015-11-25 16:09:48 -0800

    Work on queues with measure vs mutate

--- exports/ventana.js
+++ exports/ventana.js
@@ -113,16 +113,50 @@ return /******/ (function(modules) { // webpackBootstrap
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [exports, module, __webpack_require__(3), __webpack_require__(4)], __WEBPACK_AMD_DEFINE_RESULT__ = function (exports, module, _stream, _windowProxy) {
 	  'use strict';
 
+	  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	  var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 	  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-	  var _Stream = _interopRequireDefault(_stream);
+	  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-	  var RAF = new _Stream['default']('requestAnimationFrame');
+	  function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	  var _Stream2 = _interopRequireDefault(_stream);
+
+	  var RAF = new _Stream2['default']('requestAnimationFrame');
+
+	  var RAFStream = (function (_Stream) {
+	    _inherits(RAFStream, _Stream);
+
+	    function RAFStream() {
+	      _classCallCheck(this, RAFStream);
+
+	      _get(Object.getPrototypeOf(RAFStream.prototype), 'constructor', this).apply(this, arguments);
+	    }
+
+	    _createClass(RAFStream, [{
+	      key: 'write',
+	      value: function write(timestamp) {
+	        var measure = {
+	          MEASURE: true,
+	          timestamp: timestamp
+	        };
+	        var mutate = {
+	          MUTATE: true,
+	          timestamp: timestamp
+	        };
+	        _get(Object.getPrototypeOf(RAFStream.prototype), 'write', this).call(this, measure);
+	        _get(Object.getPrototypeOf(RAFStream.prototype), 'write', this).call(this, mutate);
+	      }
+	    }]);
+
+	    return RAFStream;
+	  })(_Stream2['default']);
 
 	  var pollForAF = function pollForAF() {
-	    RAF.write({
-	      timestamp: Date.now()
-	    });
+	    RAF.write(Date.now());
 	    (0, _windowProxy.rAF)(pollForAF);
 	  };
 	  (0, _windowProxy.rAF)(pollForAF);
@@ -267,15 +301,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	  var w = (0, _windowProxy.getWidth)();
 
 	  module.exports = _RAF['default'].filter(function (frame) {
-	    var nH = (0, _windowProxy.getHeight)();
-	    var nW = (0, _windowProxy.getWidth)();
-	    if (nW !== w || nH !== h) {
-	      h = nH;
-	      w = nW;
-	      return {
-	        width: w,
-	        height: h
-	      };
+	    if (frame.MEASURE) {
+	      var nH = (0, _windowProxy.getHeight)();
+	      var nW = (0, _windowProxy.getWidth)();
+	      if (nW !== w || nH !== h) {
+	        h = nH;
+	        w = nW;
+	        return {
+	          width: w,
+	          height: h
+	        };
+	      }
 	    }
 	  });
 	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
